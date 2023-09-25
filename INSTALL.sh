295200d0bdb9478b285ffbb18073cebbd350a088
erfan <75321483+erfanbanaei@users.noreply.github.com>
2023-09-25 22:14:10 +0330

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,14 +1,1 @@
-sudo apt install figlet 
-sudo apt install toilet
-sudo apt install cowsay
-sudo apt install sl
-sudo apt install fortune
-sudo apt install xcowsay
-sudo apt install cmatrix
-sudo apt install oneko
-sudo apt install espeak
-sudo apt install libaa-bin
-sudo apt install linux_log
-sudo apt install neofetch
-sudo apt install sysvbanner
-sudo apt install lolcat
+sudo apt install ncal figlet toilet cowsay sl fortune-mod xcowsay oneko espeak libaa-bin linuxlogo neofetch sysvbanner lolcat cmatrix -y
